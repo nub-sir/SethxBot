89aa837d24badab2065e397ad8bd16a8533526f1
nub-sir <77061502+nub-sir@users.noreply.github.com>
2021-04-11 13:47:35 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-@echo off
-TITLE Saitama Robot
-:: Enables virtual env mode and then starts saitama
-env\scripts\activate.bat && py -m SaitamaRobot
+@echo off 
+TITLE SETH BOT
+:: Enables virtual env mode and then starts seth
+env\scripts\activate.bat && py -m SETHxBot
