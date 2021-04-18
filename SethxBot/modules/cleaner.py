e3232e6d6fc006ef0dc20e0ffdf978baefc7d06b
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-18 19:18:40 +0530

    ..

--- SethxBot/modules/cleaner.py
+++ SethxBot/modules/cleaner.py
@@ -1,14 +1,14 @@
 import html
 
-from SaitamaRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from SethxBot import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from SethxBot.modules.disable import DisableAbleCommandHandler
+from SethxBot.modules.helper_funcs.chat_status import (
     bot_can_delete,
     connection_status,
     dev_plus,
     user_admin,
 )
-from SaitamaRobot.modules.sql import cleaner_sql as sql
+from SethxBot.modules.sql import cleaner_sql as sql
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
