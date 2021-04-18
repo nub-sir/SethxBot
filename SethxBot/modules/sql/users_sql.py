ba4f9d33c4425343b3189f51070f7fda9a6cc356
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-18 15:12:51 +0530

    ..

--- SethxBot/modules/sql/users_sql.py
+++ SethxBot/modules/sql/users_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.sql import BASE, SESSION
+from SethxBot import dispatcher
+from SethxBot.modules.sql import BASE, SESSION
 from sqlalchemy import (
     Column,
     ForeignKey,
