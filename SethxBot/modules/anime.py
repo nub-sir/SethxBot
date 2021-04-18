30c1a80b53a027e80f271691c9ca7fee83cd98dd
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-18 17:37:42 +0530

    Add files via upload

--- SethxBot/modules/anime.py
+++ SethxBot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from SethxBot import dispatcher
-from SethxBot.modules.disable import DisableAbleCommandHandler
+from SaitamaRobot import dispatcher
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
