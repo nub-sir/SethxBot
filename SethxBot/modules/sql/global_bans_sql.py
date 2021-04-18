871f4d3bfd8fb54d6c4a15bca851f67715c18522
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-18 14:57:13 +0530

    ..

--- SethxBot/modules/sql/global_bans_sql.py
+++ SethxBot/modules/sql/global_bans_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from SethxBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
