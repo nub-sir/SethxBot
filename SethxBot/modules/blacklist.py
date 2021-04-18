bbd8745e86b0ffd0271678c46c9fe2498c63e6b4
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-18 18:27:32 +0530

    ..

--- SethxBot/modules/blacklist.py
+++ SethxBot/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-import SaitamaRobot.modules.sql.blacklist_sql as sql
-from SaitamaRobot import dispatcher, LOGGER
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from SaitamaRobot.modules.helper_funcs.extraction import extract_text
-from SaitamaRobot.modules.helper_funcs.misc import split_message
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.warns import warn
-from SaitamaRobot.modules.helper_funcs.string_handling import extract_time
-from SaitamaRobot.modules.connection import connected
-from SaitamaRobot.modules.sql.approve_sql import is_approved
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+import SethxBot.modules.sql.blacklist_sql as sql
+from SethxBot import dispatcher, LOGGER
+from SethxBot.modules.disable import DisableAbleCommandHandler
+from SethxBot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from SethxBot.modules.helper_funcs.extraction import extract_text
+from SethxBot.modules.helper_funcs.misc import split_message
+from SethxBot.modules.log_channel import loggable
+from SethxBot.modules.warns import warn
+from SethxBot.modules.helper_funcs.string_handling import extract_time
+from SethxBot.modules.connection import connected
+from SethxBot.modules.sql.approve_sql import is_approved
+from SethxBot.modules.helper_funcs.alternate import send_message, typing_action
 
 BLACKLIST_GROUP = 11
 
