977059ab21e1fac37c13d442611886147fb2705a
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-18 19:11:09 +0530

    ..

--- SethxBot/modules/blacklistusers.py
+++ SethxBot/modules/blacklistusers.py
@@ -1,7 +1,7 @@
-# Module to blacklist users and prevent them from using commands by @TheRealPhoenix
+# Module to blacklist users and prevent them from using commands
 import html
-import SaitamaRobot.modules.sql.blacklistusers_sql as sql
-from SaitamaRobot import (
+import SethxBot.modules.sql.blacklistusers_sql as sql
+from SethxBot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -10,12 +10,12 @@ from SaitamaRobot import (
     WOLVES,
     dispatcher,
 )
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
-from SaitamaRobot.modules.helper_funcs.extraction import (
+from SethxBot.modules.helper_funcs.chat_status import dev_plus
+from SethxBot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from SaitamaRobot.modules.log_channel import gloggable
+from SethxBot.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
