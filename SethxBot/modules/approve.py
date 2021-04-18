59d9c5b1afb04be1dce14bcbe50fe53c67212e03
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-18 15:45:22 +0530

    ..

--- SethxBot/modules/approve.py
+++ SethxBot/modules/approve.py
@@ -1,11 +1,11 @@
 import html
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot import dispatcher, DRAGONS
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
+from SethxBot.modules.disable import DisableAbleCommandHandler
+from SethxBot import dispatcher, DRAGONS
+from SethxBot.modules.helper_funcs.extraction import extract_user
 from telegram.ext import CallbackContext, run_async, CallbackQueryHandler
-import SaitamaRobot.modules.sql.approve_sql as sql
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.log_channel import loggable
+import SethxBot.modules.sql.approve_sql as sql
+from SethxBot.modules.helper_funcs.chat_status import user_admin
+from SethxBot.modules.log_channel import loggable
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.utils.helpers import mention_html
 from telegram.error import BadRequest
