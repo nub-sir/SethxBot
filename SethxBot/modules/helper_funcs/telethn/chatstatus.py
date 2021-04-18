e0e201f420572b8ad2853eabae8089209058f115
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-18 13:56:36 +0530

    ..

--- SethxBot/modules/helper_funcs/telethn/chatstatus.py
+++ SethxBot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from SaitamaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from SaitamaRobot import DRAGONS
+from SethxBot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from SethxBot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
