5aad64c44632376dabacee98cdedf10fe0341036
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-19 08:39:02 +0530

    ..

--- SethxBot/modules/reactions.py
+++ SethxBot/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from SethxBot import dispatcher
+from SethxBot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
