04924451b10ca5e57c25f73af16bf957058e4218
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-19 10:55:56 +0530

    ..

--- SethxBot/modules/userinfo.py
+++ SethxBot/modules/userinfo.py
@@ -32,7 +32,7 @@ from SethxBot.modules.sql.afk_sql import is_afk, check_afk_status
 from SethxBot.modules.sql.users_sql import get_user_num_chats
 from SethxBot.modules.helper_funcs.chat_status import sudo_plus
 from SethxBot.modules.helper_funcs.extraction import extract_user
-from SethxBot import telethn as SaitamaTelethonClient, TIGERS, DRAGONS, DEMONS
+from SethxBot import telethn as SethTelethonClient, TIGERS, DRAGONS, DEMONS
 
 
 def no_by_per(totalhp, percentage):
@@ -160,7 +160,7 @@ def get_id(update: Update, context: CallbackContext):
             )
 
 
-@SaitamaTelethonClient.on(
+@SethTelethonClient.on(
     events.NewMessage(
         pattern="/ginfo ", from_users=(TIGERS or []) + (DRAGONS or []) + (DEMONS or [])
     )
