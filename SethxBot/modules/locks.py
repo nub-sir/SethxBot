ca8811d8bc0e9bfbde916871ddc4f1a4e5c3530e
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-19 09:08:26 +0530

    ..

--- SethxBot/modules/locks.py
+++ SethxBot/modules/locks.py
@@ -9,20 +9,20 @@ from telegram.utils.helpers import mention_html
 
 from alphabet_detector import AlphabetDetector
 
-import SaitamaRobot.modules.sql.locks_sql as sql
-from SaitamaRobot import dispatcher, DRAGONS, LOGGER
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+import SethxBot.modules.sql.locks_sql as sql
+from SethxBot import dispatcher, DRAGONS, LOGGER
+from SethxBot.modules.disable import DisableAbleCommandHandler
+from SethxBot.modules.helper_funcs.chat_status import (
     can_delete,
     is_user_admin,
     user_not_admin,
     is_bot_admin,
     user_admin,
 )
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.connection import connected
-from SaitamaRobot.modules.sql.approve_sql import is_approved
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from SethxBot.modules.log_channel import loggable
+from SethxBot.modules.connection import connected
+from SethxBot.modules.sql.approve_sql import is_approved
+from SethxBot.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
