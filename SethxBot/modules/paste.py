cd259704e9de3bbec6d3588296dffff47cc53e54
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-19 08:46:14 +0530

    ..

--- SethxBot/modules/paste.py
+++ SethxBot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from SethxBot import dispatcher
+from SethxBot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
