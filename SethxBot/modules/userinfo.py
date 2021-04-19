c0b1411deac9a74aed200c8d61e192f5cc681435
Jacky_xD <77061502+nub-sir@users.noreply.github.com>
2021-04-19 14:19:02 +0530

    ..

--- SethxBot/modules/userinfo.py
+++ SethxBot/modules/userinfo.py
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchUpdates/155">?</a>]'.format(
+        text += ' [<a href="https://t.me/SETH_UPDATES">?</a>]'.format(
             bot.username
         )
 
@@ -539,7 +539,7 @@ Examples:
  • `/info`*:* get information about a user. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/OnePunchUpdates/192)
+ Come and see [HP System explained](https://t.me/SETH_UPDATES)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
